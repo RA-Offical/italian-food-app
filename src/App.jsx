5b158e56d47c3f5e39374141beb4c2e54f59a8a7
Rashid Ameer <R.Ameer.Offical@gmail.com>
2023-09-01 22:23:49 +0500

    Started italian-food-app project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+	return <></>;
+}
+
+export default App;
