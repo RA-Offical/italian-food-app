72aec73118baf38f26d6a5577f34983b6ff8d694
Rashid Ameer <R.Ameer.Offical@gmail.com>
2023-09-19 19:58:20 +0500

    Reset Project base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<h1 className="text-slate-400 bg-fuchsia-100">
+				Hello Rashid Ameer
+			</h1>
+		</div>
+	);
 }
 
 export default App;
